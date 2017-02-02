95e503000308298b5afae8fae2878c2230b33f87
Martin Larsson <martin@wonderlab.se>
2017-02-02 09:02:35 +0100

    update dependencies to latest versions

--- src/redis4netTests/Layout/LogMessageLayoutTests.cs
+++ src/redis4netTests/Layout/LogMessageLayoutTests.cs
@@ -1,9 +1,4 @@
-﻿using log4net;
-using log4net.Appender;
-using log4net.Core;
-using log4net.Repository;
-using log4net.Util;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -13,10 +8,15 @@ using System.IO;
 
 namespace redis4netTests.Layout
 {
-	using redis4net;
-	using redis4net.Layout;
+    using log4net;
+    using log4net.Appender;
+    using log4net.Core;
+    using log4net.Repository;
+    using log4net.Util;
+    using redis4net;
+    using redis4net.Layout;
 
-	[TestFixture]
+    [TestFixture]
 	public class LogMessageLayoutTests
 	{
 		[Test]
@@ -216,8 +216,8 @@ namespace redis4netTests.Layout
 			var result = GetMessage(layout, loggingEvent);
 
 			Assert.AreEqual(message, result.Message);
-			Assert.IsNotNullOrEmpty(result.Line);
-			Assert.IsNotNullOrEmpty(result.File);
+			Assert.IsNotEmpty(result.Line);
+			Assert.IsNotEmpty(result.File);
 		}
 
 		private LogMessage GetMessage(LogMessageLayout layout, LoggingEvent message)
